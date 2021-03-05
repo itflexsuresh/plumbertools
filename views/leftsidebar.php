b969162ecffbf409da52e843407c2afbe76fa7ac
manikandanRJSM <rmanikandanrjsm@outlook.com>
2021-03-05 07:38:37 -0800

    css files for plumber tools live issue

--- views/leftsidebar.php
+++ views/leftsidebar.php
@@ -17,8 +17,11 @@ $writepermission  = explode(',', $userdetails['write_permission']);
       <link href="<?php echo base_url(); ?>assets/css/animate.min.css" rel="stylesheet"/>
       <link href="<?php echo base_url(); ?>assets/css/light-bootstrap-dashboard.css?v=1.4.0" rel="stylesheet"/>
       <link href="<?php echo base_url(); ?>assets/css/demo.css" rel="stylesheet" />
-      <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-      <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+      <!-- <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+      <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'> -->
+
+      <link href="<?php echo base_url(); ?>assets/css/font-awesome.min.css" rel="stylesheet" />
+      <link href='<?php echo base_url(); ?>assets/css/fonts.css/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
       <link href="<?php echo base_url(); ?>assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
       <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/responsive.dataTables.min.css">
       <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/jquery.dataTables.min.css">
